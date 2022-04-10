047848bc0bf17a781896108f0c728eac55613470
BrycenLikesCoding <93868711+BrycenLikesCoding@users.noreply.github.com>
2022-04-10 16:38:23 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from deta import Deta
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+
+global detaInformation
+
+detaInformation = {
+    'key': 'b08vglli_aRZbmhfWKATs54djaSHiZrFu2cFqJn19',
+    'id': 'b08vglli'
+}
+
+def initDeta():
+	deta = Deta(detaInformation['key'])
+	return deta
+
+deta = initDeta()
+
+users = deta.Base("users")
+videos = deta.Base("videos")
+thumbnails = deta.Drive("thumbnails")
+
+thumbnails.put('/thumbnails/test_thumbnail_1.jpg', path='./thumb_test')
